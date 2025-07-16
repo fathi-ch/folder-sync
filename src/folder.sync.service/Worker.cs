37570fca5c44287f6fce99ee87c89193b1b7572e
Fathi Chabane <chbnfathi@hotmail.com>
2025-07-16 16:10:22 +0200

    Update Worker.cs

--- src/folder.sync.service/Worker.cs
+++ src/folder.sync.service/Worker.cs
@@ -1,3 +1,4 @@
+using folder.sync.service.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -6,17 +7,19 @@ namespace folder.sync.service;
 public class Worker : BackgroundService
 {
     private readonly ILogger<Worker> _logger;
+    private readonly FolderSyncServiceConfig _config;
 
-    public Worker(ILogger<Worker> logger)
+    public Worker(ILogger<Worker> logger, FolderSyncServiceConfig config)
     {
         _logger = logger;
+        _config = config; 
     }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
         while (!stoppingToken.IsCancellationRequested)
         {
-            _logger.LogInformation("Worker running at: {Time}", DateTimeOffset.Now);
+            _logger.LogInformation("Worker running at: {Time}, polling every {config}", DateTimeOffset.Now, _config.IntervalInSec);
             await Task.Delay(1000, stoppingToken);
         }
     }
