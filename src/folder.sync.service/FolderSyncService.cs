fedac1613df3f02ce877ea42a8818f50595e1781
Fathi Chabane <chbnfathi@hotmail.com>
2025-07-16 23:00:41 +0200

    Create FolderSyncService.cs

---
+++ src/folder.sync.service/FolderSyncService.cs
@@ -0,0 +1,46 @@
+using folder.sync.service.Configuration;
+using folder.sync.service.Infrastructure;
+
+namespace folder.sync.service;
+
+public class FolderSyncService : BackgroundService
+{
+    private readonly ILogger<FolderSyncService> _logger;
+    private readonly IFolderSyncPipeline _folderSyncPipeline;
+    private readonly FolderSyncServiceConfig _config;
+
+    public FolderSyncService(ILogger<FolderSyncService> logger, FolderSyncServiceConfig config,
+        IFolderSyncPipeline folderSyncPipeline)
+    {
+        _logger = logger;
+        _config = config;
+        _folderSyncPipeline = folderSyncPipeline;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        var timer = new PeriodicTimer(TimeSpan.FromSeconds(_config.IntervalInSec));
+
+        try
+        {
+            while (await timer.WaitForNextTickAsync(stoppingToken))
+            {
+                await _folderSyncPipeline.RunAsync(stoppingToken);
+            }
+        }
+        catch (Exception ex)
+        {
+            var flattened = $"{ex.GetType().Name}: {ex.Message}";
+            _logger.LogError("Error: {Type} | Msg: {Message} | Stack: {Stack}",
+                ex.GetType().Name,
+                ex.Message,
+                ex.StackTrace?.Split('\n').FirstOrDefault()?.Trim());
+        }
+    }
+
+    public override async Task StopAsync(CancellationToken cancellationToken)
+    {
+        await _folderSyncPipeline.StopAsync();
+        await base.StopAsync(cancellationToken);
+    }
+}
