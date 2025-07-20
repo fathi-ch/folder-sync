7ef7dd6a26e89b7d72ae79433b462a9385b41874
Fathi Chabane <chbnfathi@hotmail.com>
2025-07-21 01:46:22 +0200

    Update Program.cs

--- src/folder.sync.service/Program.cs
+++ src/folder.sync.service/Program.cs
@@ -15,7 +15,7 @@ using folder.sync.service.Infrastructure.State;
 using folder.sync.service.Service;
 using folder.sync.service.Common;
 
-var builder = Host.CreateApplicationBuilder();
+var builder = WebApplication.CreateBuilder();
 
 builder.Services.Configure<HostOptions>(opts =>
 {
@@ -42,19 +42,16 @@ try
     }
     else
     {
-        syncOptions = builder.Configuration
-                          .GetSection(FolderSyncServiceConfig.SectionName)
-                          .Get<FolderSyncServiceConfig>() ??
-                      throw new InvalidOperationException($"Missing section: {FolderSyncServiceConfig.SectionName}");
+        syncOptions = GetSectionValue<FolderSyncServiceConfig>(builder.Configuration, FolderSyncServiceConfig.SectionName);
     }
 
     syncOptions.Validate();
 
-    LoggerSetup(builder, syncOptions);
-
-    builder.Logging.AddSerilog(Log.Logger, true);
+    builder.Host.UseSerilog((ctx, sp, lc) => { ConfigureLogger(lc, ctx, syncOptions); });
+    
     builder.Services.AddSingleton(syncOptions);
     builder.Services.AddHostedService<FolderSyncService>();
+    builder.Services.AddHealthChecks();
     builder.Services.AddSingleton<IFolderSyncPipeline, FolderSyncPipeline>();
     builder.Services.AddSingleton<IFileLoader, FileSystemLoader>();
     builder.Services.AddSingleton<ISyncLabeler, SyncLabeler>();
@@ -73,9 +70,10 @@ try
         _ => new ConcurrentCommandExecutor(4));
     builder.Services.AddSingleton<IBatchState, BatchState>();
     builder.Services.AddSingleton<ISyncTaskConsumer, BatchSyncTaskConsumer>();
-  //  builder.Services.AddSingleton<ISyncTaskConsumer, SyncTaskConsumer>();
+  //  builder.Services.AddSingleton<ISyncTaskConsumer, SyncTaskConsumer>(); //Useful for quick testing
 
     var app = builder.Build();
+    app.MapHealthChecks("/healthz");
     var log = app.Services.GetRequiredService<ILogger<Program>>();
     log.LogInformation("Service startup complete");
     app.Run();
@@ -85,16 +83,29 @@ catch (Exception ex)
     Log.Logger.Fatal(ex, "Fatal error occurred during startup or runtime");
 }
 
-void LoggerSetup(HostApplicationBuilder hostApplicationBuilder, FolderSyncServiceConfig folderSyncServiceConfig)
+return;
+
+T GetSectionValue<T>(IConfiguration configuration, string sectionName) where T : class
 {
-    Log.Logger = new LoggerConfiguration()
-        .Enrich.With<ShortSourceContextEnricher>()
-        .ReadFrom.Configuration(hostApplicationBuilder.Configuration)
-        .WriteTo.File(
-            folderSyncServiceConfig.LogPath,
-            rollingInterval: RollingInterval.Day,
-            retainedFileCountLimit: 14,
-            outputTemplate: AppConstants.SerilogTemplateOutPut
-        )
-        .CreateLogger();
+    return configuration
+               .GetSection(sectionName)
+               .Get<T>()
+           ?? throw new InvalidOperationException($"Missing section: {sectionName}");
 }
+
+void ConfigureLogger(LoggerConfiguration loggerConfiguration, HostBuilderContext hostBuilderContext,
+    FolderSyncServiceConfig folderSyncServiceConfig)
+{
+    loggerConfiguration.ReadFrom.Configuration(hostBuilderContext.Configuration)
+        .Enrich.With<ShortSourceContextEnricher>()
+        .Enrich.WithThreadId()
+        .Enrich.WithMachineName();
+
+    loggerConfiguration.WriteTo.File(
+        folderSyncServiceConfig.LogPath,
+        rollingInterval: RollingInterval.Day,
+        retainedFileCountLimit: 14,
+        outputTemplate: AppConstants.SerilogTemplateOutPut
+    );
+}
+
